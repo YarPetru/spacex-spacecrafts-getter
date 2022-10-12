6b159d9cdec40cb3f601d228c8253b7f60fac31e
Yaroslav Petrukhin <yar.petrukhin@gmail.com>
2022-10-12 21:40:57 +0300

    background image optimized, reserve fonts added

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -16,10 +16,14 @@ export const GlobalStyle = createGlobalStyle`
      background-size: cover;
     margin: 0;
     padding: 0;
-    font-family: 'Roboto Mono', monospace;
+    font-family: 'Roboto Mono', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+    'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    monospace;
     font-style: normal;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    color: #242a37;
+    background-color: #091e3f;
     }
 
     code {
